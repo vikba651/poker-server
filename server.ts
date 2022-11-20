f4582ab558aeee2a9abaa0f28fd22d7cb85c007a
Viktor Barr <viktor.barr@netlight.com>
2022-11-20 21:28:10 +0100

    Use socket rooms

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import { createServer } from 'http'
 import { Server, Socket } from 'socket.io'
 import * as mongoose from 'mongoose'
 import * as bodyParser from 'body-parser'
+import { networkInterfaces } from 'os'
 
 import roundsRouter from './routes/rounds'
 import playersRouter from './routes/players'
@@ -13,17 +14,26 @@ dotenv.config()
 const app: Express = express()
 const server = createServer(app)
 
+const nets = networkInterfaces()
+let localAddr: string
+
+try {
+  localAddr = nets.en0?.find((net) => net.family === 'IPv4')?.address ?? 'localhost'
+} catch (error) {
+  console.log('Could not get local IP')
+}
+
+const port: number = process.env.PORT ? +process.env.PORT : 8999
+server.listen(port, () => {
+  console.log(`Server started on http://${localAddr}:${port} :)`)
+})
+
 mongoose.connect(`${process.env.DATABASE_URL}`, (error) => {
   if (error) {
     console.log('Could not connect to Mongoose')
   } else {
     console.log('Connected to Mongoose')
   }
-})
-
-const port: number = process.env.PORT ? +process.env.PORT : 8999
-server.listen(port, () => {
-  console.log(`Server started on http://localhost:${port} :)`)
 })
 
 // ****** HTTP STUFF ******
@@ -50,36 +60,36 @@ wss.on('connection', (ws: Socket) => {
   // console.log("addr", ws.handshake.address);
   ws.on('createSession', (data) => {
     console.log(`${data.name} wants to create a session`)
-    const newSession = {
+    const session: Session = {
       id: sessions.length,
       code: createCode(),
       creator: data.name,
       players: [data.name],
     }
-    sessions.push(newSession)
-
-    ws.emit('sessionCreated', newSession)
+    sessions.push(session)
+    ws.join(session.id.toString())
+    ws.emit('sessionCreated', session)
   })
 
   ws.on('joinSession', (data) => {
     let session = sessions.find((session) => session.code === data.code)
     if (session) {
       session.players.push(data.name)
-      wss.emit('sessionUpdated', session)
+      ws.join(session.id.toString())
+      wss.to(session.id.toString()).emit('sessionUpdated', session)
     } else {
       ws.send('Invalid session code')
     }
   })
 })
 
+let sessions: Session[] = []
 interface Session {
   id: number
   code: string
   creator: string
   players: string[]
 }
-
-let sessions: Session[] = []
 
 const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
