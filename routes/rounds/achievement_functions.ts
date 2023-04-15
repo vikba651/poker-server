67bcbd492055748f3c9006dc50760824263c36aa
Martin Brolin <martinbrolin97@gmail.com>
2023-04-15 20:04:00 +0200

    Flutch Score

--- routes/rounds/achievement_functions.ts
+++ routes/rounds/achievement_functions.ts
@@ -1,7 +1,7 @@
 import { DealSummary } from '../../types/statistics'
 import { getHandResult } from '../../statistics/poker-logic'
 
-export function StraightClutchScore(name: string, dealSummaries: DealSummary[]) {
+export function straightClutchScore(name: string, dealSummaries: DealSummary[]) {
   let score = 0
   for (const dealSummary of dealSummaries) {
     const playerCards = dealSummary.playerCards.find((playerCards) => {
@@ -12,6 +12,25 @@ export function StraightClutchScore(name: string, dealSummaries: DealSummary[]) 
         continue
       }
       if (getHandResult([...playerCards.cards, ...dealSummary.tableCards.slice(0, 4)]).hand == 'Straight') {
+        continue
+      }
+      score += 1
+    }
+  }
+  return score
+}
+
+export function flutchScore(name: string, dealSummaries: DealSummary[]) {
+  let score = 0
+  for (const dealSummary of dealSummaries) {
+    const playerCards = dealSummary.playerCards.find((playerCards) => {
+      return playerCards.name == name
+    })
+    if (playerCards) {
+      if (playerCards.hand !== 'Flush') {
+        continue
+      }
+      if (getHandResult([...playerCards.cards, ...dealSummary.tableCards.slice(0, 4)]).hand == 'Flush') {
         continue
       }
       score += 1
